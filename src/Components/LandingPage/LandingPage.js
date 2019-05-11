1693082874349fdb1140a90e07831d7908018928
Lili Bourgeois <lbourgeois90@gmail.com>
2019-05-11 17:02:45 -0500

    animation on landing page

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,5 +1,11 @@
 import React, { Component} from 'react';
-import './LandingPage.css'
+import './LandingPage.css';
+import LaptopMac from '@material-ui/icons/LaptopMac';
+import Face from '@material-ui/icons/Face';
+import School from '@material-ui/icons/School';
+import Favorite from '@material-ui/icons/Favorite';
+import LocalFlorist from '@material-ui/icons/LocalFlorist';
+import MoreHoriz from '@material-ui/icons/MoreHoriz';
 
 class LandingPage extends Component {
   
@@ -11,26 +17,44 @@ class LandingPage extends Component {
        <section className="landingPageWrapper">
 
         <div className="nameDiv">
-            <h1 className="meetH1">Meet</h1>
-            <h1 className="liliH1">Lili</h1>
+            <h1 className="meetH1">Meet
+            <br/> Lili</h1>
         </div>
 
         <div className="picDiv">
-            <img src="/images/teacher_outline.png"></img>
+            <p><img src="/images/teacher_outline.png"></img></p>
         </div>
 
         <div className="aboutMeDiv">
-            <h1 id="helloText">Hello.</h1>
-            <p>People call me Lili.</p>
-            <p>I am currently studying Full Stack Software Engineering at Prime Digital Academy.</p>
-            <p>I used to be a high school teacher.</p>
-            <p>Now I type letters and numbers to "write" code.</p>
-            <p>I like when things are pretty.</p>
-            <p>Learn more about <a href="">Me</a></p> 
-        
+            <h1 id="animatedHello">
+                <span>H</span>
+                <span>E</span>
+                <span>L</span>
+                <span>L</span>
+                <span>O</span>
+            </h1>
+            <p><Face/> People call me Lili.</p>
+            <p><School/> I am currently studying Full Stack Software Engineering at Prime Digital Academy.</p>
+            <p><Favorite/> I used to be a high school teacher.</p>
+            <p><LaptopMac/> Now I type letters and numbers to "write" code.</p>
+            <p><LocalFlorist/> I like when things are pretty.</p>
+            <p><MoreHoriz/> Learn more about <a href="/#/resume">Me</a></p> 
+ 
+        </div>
 
-    
-            
+        <div className="contactDiv">
+        <h1 id="animatedContact">
+                <span>C</span>
+                <span>O</span>
+                <span>N</span>
+                <span>T</span>
+                <span>A</span>
+                <span>C</span>
+                <span>T</span>
+            </h1>
+            <a href="https://github.com/lbourgeois90" target="_blank"><img src="/images/githubicon.png"></img></a>
+            <a href="mailto:lbourgeois@gmail.com"><img src="/images/gmailIcon.png"></img></a>
+            <a href="https://www.linkedin.com/in/lili-bourgeois-49947563/" target="_blank"><img src="/images/linkedinicon.png"></img></a>
         </div>
          
        </section>
