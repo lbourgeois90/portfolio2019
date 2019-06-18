363d3c511467168539a397a6bea0d88c2efff4d1
Lili Bourgeois <lbourgeois90@gmail.com>
2019-06-18 15:16:05 -0500

    Updates

---
+++ precache-manifest.f4f4cff4bb832a15aa1ce0ea15a3f70d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c518389d61a8366cb85c2603a28e2815",
+    "url": "/portfolio2019/index.html"
+  },
+  {
+    "revision": "c1eea02975150c2d8762",
+    "url": "/portfolio2019/static/css/main.38b49b59.chunk.css"
+  },
+  {
+    "revision": "412db9672ed3f68070c2",
+    "url": "/portfolio2019/static/js/2.923bf0eb.chunk.js"
+  },
+  {
+    "revision": "c1eea02975150c2d8762",
+    "url": "/portfolio2019/static/js/main.27d70690.chunk.js"
+  },
+  {
+    "revision": "1b8f21f30d9826a2fafe",
+    "url": "/portfolio2019/static/js/runtime~main.8abb7632.js"
+  }
+]);
